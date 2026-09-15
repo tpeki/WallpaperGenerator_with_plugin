075dc402b281367dd0049746bde81678a5c3b3c8
TPK <121001824+tpeki@users.noreply.github.com>
2026-09-16 00:15:10 +0900

    tiles2 wip
    bg support

--- work/mod_tiles2.py
+++ work/mod_tiles2.py
@@ -92,7 +92,8 @@ def set_hist(atname, catname, value):
     if catname not in tiles_preserv:
         tiles_preserv[catname] = {}
     tiles_preserv[catname][atname] = value
-    return
+    
+    return value
 
 
 # ---
@@ -163,14 +164,14 @@ def generate(p: Param):
     org_w, org_h = p.width, p.height
     width, height = org_w, org_h
 
-    d = get_hist('tsize', 'common', p.pwidth)  #Tile size
-    r = get_hist('round', 'common', p.pheight)  # Tile radius
-    jw = get_hist('thick', 'joint', p.pdepth)
+    d = set_hist('tsize', 'common', p.pwidth)  #Tile size
+    rr = set_hist('round', 'common', p.pheight)  # Tile radius
+    jw = set_hist('thick', 'joint', p.pdepth)
     joint_width = min(max(1, int(d * jw / 100)), d-1)
-    joint_color = get_hist('color', 'joint', ((p.sub_jitter,)*3))
+    joint_color = set_hist('color', 'joint', ((p.sub_jitter,)*3))
     joint_brightness = sum(joint_color)/len(joint_color)
     
-    sand_grain_size = get_hist('grain', 'joint', SAND_GRAIN_SIZE)
+    joint_grain = get_hist('grain', 'joint', SAND_GRAIN_SIZE)
     int_bdr = get_hist('border', 'joint', INT_BDR)
     int_brt = get_hist('bright', 'joint', INT_BRT)
     int_drk = get_hist('dark', 'joint', INT_DRK)
@@ -181,13 +182,14 @@ def generate(p: Param):
         sand_intensity = 0
         sand_int_add = ((int_bdr-joint_brightness)/int_bdr)**2 * int_drk
 
-    jitter = get_hist('jitter', 'common', p.color_jitter)
 
     colors = tiles_preserv['colors']
     colors[0] = p.color1.ctoi()
     colors[1] = p.color2.ctoi()
     colors[2] = p.color3.ctoi()
+    tiles_preserv['colors'] = colors
     num_colors = get_hist('ncolor', 'common', 3)
+    jitter = set_hist('jitter', 'common', p.color_jitter)
 
     persmode = get_hist('3d', 'common', 0) == 1
     pers_str = get_hist('pers', 'common', PERS)
@@ -202,8 +204,14 @@ def generate(p: Param):
         width, height = pre_rotate_size(width, height, angle)
 
     # 1. ベース作成（目地色）
-    img_array = np.full((height, width, 3), joint_color, dtype=np.float32)
-    grain_mask = np.random.rand(height, width) < sand_grain_size
+    baseimg = p.bg(width, height)
+    if baseimg is None:
+        baseimg = Image.new('RGB', (width, height), joint_color)
+    else:
+        baseimg = baseimg.convert('RGB')
+
+    img_array = np.asarray(baseimg, dtype=np.float32)
+    grain_mask = np.random.rand(height, width) < joint_grain
     img_array[grain_mask] *= sand_intensity
     img_array[grain_mask] += sand_int_add
     
@@ -216,7 +224,7 @@ def generate(p: Param):
     offset_x = (width - total_w) // 2
 
     # タイルごとの色インデックスを記録
-    color_indices = np.full((rows, cols), -1, dtype=int)
+    color_idx = np.full((rows, cols), -1, dtype=int)
 
     # --- 共通データの事前計算 ---
     ty, tx = np.meshgrid(np.arange(tile_size),
@@ -224,20 +232,28 @@ def generate(p: Param):
     
     # 角丸マスク
     mask_full = np.ones((tile_size, tile_size), dtype=bool)
-    corners = [(r, r), (r, tile_size-1-r), (tile_size-1-r, r),
-               (tile_size-1-r, tile_size-1-r)]
-    for cy, cx in corners:
+    tr = min(max(0, int(tile_size * rr / 100)), tile_size//2)
+    #print(f'tile:{d} round:{rr}% -> {tr}')
+    
+    corners = [(tr, tr), (tr, tile_size-1-tr), (tile_size-1-tr, tr),
+               (tile_size-1-tr, tile_size-1-tr)]
+    for i, (cy, cx) in enumerate(corners):
         dist_sq = (ty - cy)**2 + (tx - cx)**2
-        if cy == r and cx == r: region = (ty < r) & (tx < r)
-        elif cy == r: region = (ty < r) & (tx > cx)
-        elif cy > r and cx == r: region = (ty > cy) & (tx < r)
-        else: region = (ty > cy) & (tx > cx)
-        mask_full[region] = dist_sq[region] <= r**2
+        if i == 0:
+            region = (ty < tr) & (tx < tr)
+        elif i == 1:
+            region = (ty < tr) & (tx > cx)
+        elif i == 2:
+            region = (ty > cy) & (tx < tr)
+        else:
+            region = (ty > cy) & (tx > cx)
+        mask_full[region] = dist_sq[region] <= tr**2
 
     # グラデーションマップ
-    grad_map = ((ty + tx) / ((tile_size - 1) * 2)).astype(np.float32)[:, :, np.newaxis]
+    grad_map = ((ty + tx) / ((tile_size - 1) * 2)
+                ).astype(np.float32)[:, :, np.newaxis]
     line_base = (ty + tx).astype(np.float32)
-    line_interval = d / LINTERVAL
+    line_interval = d / get_hist('pitch', 'scratchedsand', LINTERVAL)
 
     # --- 各タイルの描画 ---
     for row in range(rows):
@@ -256,35 +272,40 @@ def generate(p: Param):
             if num_colors == 2:  # 2色の場合は市松模様
                 c_idx = (row + col) % 2
             else:  # 3色以上の場合は、同色3枚隣接禁止のランダム
-                possible_indices = list(range(num_colors))
-                for e in ((-2,0), (-1,-1), (-1,1)):
-                    if (0 <= col+e[1] < cols) and \
-                       0 <= row+e[0] and \
-                       color_indices[row-1, col] == color_indices[row+e[0], col+e[1]]:
-                        invalid = color_indices[row-1, col]
-                        if invalid in possible_indices:
-                            possible_indices.remove(invalid)
-                for e in ((-1,-1), (-1,0), (0,-2)):
-                    if 0 <= col+e[1] and \
-                       (0 <= row+e[0] < rows) and \
-                       color_indices[row, col-1] == color_indices[row+e[0], col+e[1]]:
-                        invalid = color_indices[row, col-1]
-                        if invalid in possible_indices:
-                            possible_indices.remove(invalid)
+                possible_idx = list(range(num_colors))
+                for e0,e1 in ((-2,0), (-1,-1), (-1,1)):
+                    
+                    if (0 <= col+e1 < cols) and \
+                       0 <= row+e0 and \
+                       color_idx[row-1, col] == color_idx[row+e0, col+e1]:
+                        invalid = color_idx[row-1, col]
+                        if invalid in possible_idx:
+                            possible_idx.remove(invalid)
+                for e0,e1 in ((-1,-1), (-1,0), (0,-2)):
+                    if 0 <= col+e1 and \
+                       (0 <= row+e0 < rows) and \
+                       color_idx[row, col-1] == color_idx[row+e0, col+e1]:
+                        invalid = color_idx[row, col-1]
+                        if invalid in possible_idx:
+                            possible_idx.remove(invalid)
                             
-                c_idx = np.random.choice(possible_indices)
+                c_idx = np.random.choice(possible_idx)
 
-            color_indices[row, col] = c_idx
+            color_idx[row, col] = c_idx
             base_color = np.array(colors[c_idx], dtype=np.float32)
             
             # グラデーション
-            tile_rgb = base_color * (1.0 - grad_map[sy, sx] * GRAD_STR)
+            grad = get_hist('grad', 'scratchedsand', GRAD_STR)
+            tile_rgb = base_color * (1.0 - grad_map[sy, sx] * grad)
             # 1.0 -> 0.7
 
             # ラインテクスチャ
+            grain = get_hist('grain', 'scratchedsand', NOISE_THICK)
+            dens = get_hist('density', 'scratchedsand', NOISE_RATE)
+            
             offset = np.random.rand() * line_interval
-            line_mask = ((line_base[sy, sx] + offset) % line_interval) < NOISE_THICK
-            noise = np.random.rand(dy1-dy0, dx1-dx0) > NOISE_RATE
+            line_mask = ((line_base[sy, sx] + offset) % line_interval) < grain
+            noise = np.random.rand(dy1-dy0, dx1-dx0) > dens
             
             # ラインの色塗り (tile_rgbを直接書き換え)
             line_rgb = base_color * np.random.uniform(0.7, 0.9)
